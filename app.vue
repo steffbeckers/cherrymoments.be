417e13c1ffc87c543503a17591783b3e6ca6a54a
Steff Beckers <steff@steffbeckers.eu>
2023-05-16 22:53:14 +0200

    Cherry Moments title

--- app.vue
+++ app.vue
@@ -1,5 +1,25 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="wrapper">
+    <h1>Cherry Moments</h1>
   </div>
 </template>
+
+<style>
+html, body, #__nuxt, .wrapper {
+    height: 100%;
+    margin: 0px;
+    padding: 0px;
+    width: 100%;
+}
+
+body {
+    font-family: Helvetica, Arial, sans-serif;
+}
+
+.wrapper {
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+</style>
