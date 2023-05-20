c6ec8a79d3e107ec0923fa984e885335eb1d8655
Steff Beckers <steff@steffbeckers.eu>
2023-05-20 13:19:25 +0200

    Template assets added, tailwind and google fonts removed

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,20 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  css: ["@/assets/scss/reset.css", "@/assets/scss/main.scss"],
-  modules: [
-    [
-      "@nuxtjs/google-fonts",
-      {
-        families: {
-          Lato: true,
-          download: true,
-          inject: true,
-        },
-        prefetch: true,
-      },
-    ],
-    "@nuxtjs/tailwindcss",
-  ],
   app: {
     head: {
       title: "Cherry Moments",
