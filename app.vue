f263a16b5d939ca631fb7ffda2a7a7a5cf37c47f
Steff Beckers <steff@steffbeckers.eu>
2023-06-01 08:26:02 +0200

    Reset to Cherry Moments title

--- app.vue
+++ app.vue
@@ -1,5 +1,20 @@
 <template>
-  <NuxtLayout>
+  <div class="d-flex align-items-center justify-content-center h-100">
+    <div>
+      <h1>Cherry Moments</h1>
+      <p>Moments to cherish</p>
+    </div>
+  </div>
+  <!-- TODO -->
+  <!-- <NuxtLayout>
     <NuxtPage />
-  </NuxtLayout>
+  </NuxtLayout> -->
 </template>
+
+<style>
+html,
+body,
+#__nuxt {
+  height: 100%;
+}
+</style>
